b3649d495cba62b469314b25294bfd734d8c27eb
civilianrebel <steventayloroo@gmail.com>
2020-01-02 01:33:17 -0500

    added some helper functions

--- database.py
+++ database.py
@@ -8,12 +8,20 @@ class FileStorage:
 
     def __getitem__(self, item):
         if type(item) is str:
-            return self.get_base(item)
+            return self.get_host(item)
         if type(item) is tuple:
             # must be host and url
-            return self.get_direct(item)
+            return self.get_url(item)
         else:
-            raise IndexError
+            raise TypeError
 
     def __setitem__(self, key, value):
         pass
+
+    def get_host(self, host_str):
+        with open(self.host_file(host_str), 'r') as file:
+            return Host(json.load(file))
+
+    def get_url(self, url_tuple):
+        with open(self.url_file(url_tuple), 'r') as file:
+            return Url(json.load(file))
