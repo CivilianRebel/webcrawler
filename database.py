a2a9359d903c8fe7c9c199a89347ae36ba5c6d3a
civilianrebel <steventayloroo@gmail.com>
2020-01-02 13:49:36 -0500

    finished file storage functionality

--- database.py
+++ database.py
@@ -13,17 +13,17 @@ class FileStorage:
         if type(item) is str:
             return self.get_host(item)
         if type(item) is tuple:
-            # must be host and url
+            # must be host and url (host, url)
             return self.get_url(item)
         else:
             raise TypeError
 
     def __setitem__(self, key, value):
         if type(item) is str:
-            return self.set_host(item, value)
+            self.set_host(item, value)
         if type(item) is tuple:
             # must be host and url
-            return self.set_url(item, value)
+            self.set_url(item, value)
         else:
             raise TypeError
 
@@ -44,3 +44,24 @@ class FileStorage:
         assert type(value) is Url, 'Type must be Url'
         with open(self.url_file(url_tuple), 'r') as file:
             json.dump(file, value.dump)
+
+    def url_file(self, url_tuple):
+        f = self.root
+        if not os.path.exists(f):
+            os.mkdir(f)
+        if not os.path.exists(os.path.join(f, url_tuple[0])):
+            os.mkdir(os.path.join(f, url_tuple[0]))
+        f = os.path.join(f, url_tuple[0], url_tuple[1])
+        if not os.path.exists(f):
+            os.mkdir(f)
+        f = os.path.join(f, 'url_data.json')
+        return f
+
+    def host_file(self, host_str):
+        f = self.root
+        if not os.path.exists(f):
+            os.mkdir(f)
+        if not os.path.exists(os.path.join(f, host_str)):
+            os.mkdir(os.path.join(f, host_str))
+        f = os.path.join(f, host_str, 'host_data.json')
+        return f
